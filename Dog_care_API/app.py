5da9e58b3a39b8fcc33231a92ee7fc0a84d20c2b
marturoch <marturoch@gmail.com>
2021-11-11 14:43:05 -0500

    cambios

--- Dog_care_API/app.py
+++ Dog_care_API/app.py
@@ -1,6 +1,7 @@
 import flask
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from models import Order
 
 
 app = Flask(__name__)
@@ -9,13 +10,41 @@ CORS(app)
 memory_users = []
 memory_orders = []
 memory_dogs = []
+order_id = 0
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
-@app.route('/api/v1/registro', methods=["GET", "POST"])
-def registrarse():
+@app.route('/api/v1/orders', methods=["GET", "POST"])
+def Orders():
+    is_create = flask.request.method == 'POST'
+    is_get_all = flask.request.method == 'GET'
+
+    if is_create:
+        body = flask.request.json
+        order_id_gen = generate_order_id()
+        new_order = Order(
+            order_id_gen,
+            body["name"],
+            body["surname"],
+            body["phoneNumber"],
+            body["location"],
+            body["postalCode"],
+            body["email"],
+            body["pedidos"]
+        )
+        memory_orders.append(new_order.to_json())
+        return {"Orden": new_order.to_json(), "mensaje": "Orden creada exitosamente", "status": "ok"}
+
+    if is_get_all:
+        return jsonify(memory_orders)
+def generate_order_id():
+    order_id.__add__(1)
+    return str((len(memory_orders) + 1)).rjust(10, "0")
+
+@app.route('/api/v1/registros', methods=["GET", "POST"])
+def registros():
     is_registered = flask.request.method == 'POST'
     is_get = flask.request.method == 'GET'
 
@@ -26,7 +55,6 @@ def registrarse():
            "user_surname": body["surname"],
            "user_mail": body["mail"],
            "user_password": body["password"],
-
        }
         memory_users.append(nuevo_registro)
         return jsonify({"registro": nuevo_registro, "mensaje": "usuario registrado correctamente", "status": "ok"})
@@ -34,46 +62,24 @@ def registrarse():
     if is_get:
         return "hola"
 
-@app.route('/api/v1/orden', methods=["GET", "POST"])
-def crearOrden():
+@app.route('/api/v1/perros', methods=["GET", "POST"])
+def Dogs():
     is_created = flask.request.method == 'POST'
     is_get = flask.request.method == 'GET'
 
     if is_created:
         body = flask.request.json
-        nueva_orden = {
-           "name": body["name"],
-           "surname": body["surname"],
-           "phoneNumber": body["phoneNumber"],
-           "location": body["location"],
-           "postalCode": body["postalCode"],
-           "email": body["email"],
-           "pedidos": body["pedidos"],
-       }
-        memory_orders.append(nueva_orden)
-        return jsonify({"Orden": nueva_orden, "mensaje": "Orden creada exitosamente", "status": "ok"})
-
-    if is_get:
-        return jsonify(memory_orders)
-
-@app.route('/api/v1/perro', methods=["GET", "POST"])
-def crearPerro():
-    is_created = flask.request.method == 'POST'
-    is_get = flask.request.method == 'GET'
-
-    if is_created:
-        body = flask.request.json
-        nuevo_perro = {
+        new_dog = {
            "name": body["name"],
            "raza": body["raza"],
            "edad": body["edad"],
            "lugar": body["lugar"],
        }
-        memory_dogs.append(nuevo_perro)
-        return jsonify({"infoPerro": nuevo_perro, "mensaje": "Perro agregado exitosamente", "status": "ok"})
+        memory_dogs.append(new_dog)
+        return jsonify({"infoPerro": new_dog, "mensaje": "Perro agregado exitosamente", "status": "ok"})
 
     if is_get:
-        return jsonify(memory_orders)
+        return jsonify(memory_dogs)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)

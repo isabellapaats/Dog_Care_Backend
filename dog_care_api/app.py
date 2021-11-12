4a359a28a85804207863be4940fdf4c95b442037
Isabella Paats <isabellapaats@hotmail.com>
2021-11-12 08:50:46 -0300

    .

--- dog_care_api/app.py
+++ dog_care_api/app.py
@@ -1,8 +1,7 @@
 import flask
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from models import Order
-
+from models import Order, User, Dog
 
 app = Flask(__name__)
 CORS(app)
@@ -15,7 +14,7 @@ order_id = 0
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'Hello World!!'
 
 
 @app.route('/api/v1/orders', methods=["GET", "POST"])
@@ -55,37 +54,41 @@ def registros():
 
     if is_registered:
         body = flask.request.json
-        nuevo_registro = {
-           "user_name": body["user"],
-           "user_surname": body["surname"],
-           "user_mail": body["mail"],
-           "user_password": body["password"],
-       }
-        memory_users.append(nuevo_registro)
-        return jsonify({"registro": nuevo_registro, "mensaje": "usuario registrado correctamente", "status": "ok"})
+        new_user = User(
+            body["name"],
+            body["surname"],
+            body["email"],
+            body["password"]
+        )
+        memory_users.append(new_user.to_json())
+        return {"Usuario": new_user.to_json(), "mensaje": "Usuario creado exitosamente", "status": "ok"}
 
     if is_get:
-        return "hola"
+        return jsonify(memory_users)
 
 
-@app.route('/api/v1/perros', methods=["GET", "POST"])
+@app.route('/api/v1/perros', methods=["GET", "POST", "POST"])
 def Dogs():
-    is_created = flask.request.method == 'POST'
+    added = flask.request.method == 'POST'
     is_get = flask.request.method == 'GET'
+    is_delete = flask.request.method == 'DELETE'
 
-    if is_created:
+    if added:
         body = flask.request.json
-        new_dog = {
-           "name": body["name"],
-           "raza": body["raza"],
-           "edad": body["edad"],
-           "lugar": body["lugar"],
-       }
-        memory_dogs.append(new_dog)
-        return jsonify({"infoPerro": new_dog, "mensaje": "Perro agregado exitosamente", "status": "ok"})
+        new_dog = Dog(
+            body["name"],
+            body["raza"],
+            body["edad"],
+            body["lugar"]
+        )
+        memory_dogs.append(new_dog.to_json())
+        return {"Perro": new_dog.to_json(), "mensaje": "Masota agregada exitosamente", "status": "ok"}
 
     if is_get:
         return jsonify(memory_dogs)
+
+    if is_delete:
+        return "no se como es esto"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- dog_care_api/models.py
+++ dog_care_api/models.py
@@ -1,5 +1,5 @@
 class Order:
-    def __init__ (self, order_id, customer_name, customer_surname, phone_number, address, postalCode, email, pedido):
+    def __init__(self, order_id, customer_name, customer_surname, phone_number, address, postalCode, email, pedido):
         self.order_id = order_id
         self.customer_name = customer_name
         self.customer_surname = customer_surname
@@ -8,6 +8,28 @@ class Order:
         self.postalCode = postalCode
         self.email = email
         self.pedido = pedido
+
+    def to_json(self):
+        return self.__dict__
+
+
+class User:
+    def __init__(self, user_name, user_surname, user_mail, user_password):
+        self.user_name = user_name
+        self.user_surname = user_surname
+        self.user_mail = user_mail
+        self.user_password = user_password
+
+    def to_json(self):
+        return self.__dict__
+
+
+class Dog:
+    def __init__(self, name, raza, edad, lugar):
+        self.name = name
+        self.raza = raza
+        self.edad = edad
+        self.lugar = lugar
 
     def to_json(self):
         return self.__dict__
